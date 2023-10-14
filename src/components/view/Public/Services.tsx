1ea865f2e97a4d506543227161754283ba688815
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-14 19:04:31 +0600

    feat: added services compoent

---
+++ src/components/view/Public/Services.tsx
@@ -0,0 +1,74 @@
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
+import { ArrowRightOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
+const Services = () => {
+  return (
+    <div className="px-10 sm:px-24 py-10 mt-10 sm:py-24">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-medium sm:text-4xl sm:font-bold font-poppins text-secondary">
+          Our Services
+        </h1>
+        <Button
+          size="large"
+          className="text-primary shadow-gray-400 shadow-sm text-sm font-semibold  flex justify-center border-none items-center"
+        >
+          View All <ArrowRightOutlined />
+        </Button>
+      </div>
+      <hr className="sm:w-36 w-24  h-1 bg-primary my-2" />
+
+      <Row
+        className="mt-10"
+        justify={"center"}
+        align={"middle"}
+        gutter={[24, 24]}
+      >
+        <Col xs={24} sm={24} md={12} lg={12} xl={8}>
+          <Link className="group" href="/">
+            <Image
+              src="/oven.jpeg"
+              alt="oben"
+              layout="responsive"
+              height={250}
+              width={300}
+            />
+            <h3 className="text-center group-hover:text-primary text-secondary text-lg font-poppins pt-1">
+              Refrigerator Repair
+            </h3>
+          </Link>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12} xl={8}>
+          <Link className="group" href="/">
+            <Image
+              src="/oven.jpeg"
+              alt="oben"
+              layout="responsive"
+              height={250}
+              width={300}
+            />
+            <h3 className="text-center group-hover:text-primary text-secondary text-lg font-poppins pt-1">
+              Refrigerator Repair
+            </h3>
+          </Link>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12} xl={8}>
+          <Link className="group" href="/">
+            <Image
+              src="/oven.jpeg"
+              alt="oben"
+              layout="responsive"
+              height={250}
+              width={300}
+            />
+            <h3 className="text-center group-hover:text-primary text-secondary text-lg font-poppins pt-1">
+              Refrigerator Repair
+            </h3>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+export default Services;
