2a2bc325049432e57a3880cc9fba0c7279c33152
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-16 21:46:17 +0600

    feat: added get all category function

---
+++ src/services/category/get-all-category.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { IUser } from "@/types/user";
+
+export const getAllCategories = async (): Promise<IUser[]> => {
+  const res = await fetch(`http://localhost:4000/api/v1/categories`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    next: {
+      revalidate: 24 * 60 * 60,
+      tags: ["all-category"],
+    },
+  });
+  const { data } = await res.json();
+  if (res.ok && data) {
+    return data;
+  } else {
+    return [];
+  }
+};
